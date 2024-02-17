25fef18669c8934d1271ffad59bbb638e00668ff
Hrishikeshan A Namboodiri <118446149+Coderpo5@users.noreply.github.com>
2024-02-17 19:59:03 +0530

    Update email.py

--- base/email.py
+++ base/email.py
@@ -5,7 +5,11 @@ from django.utils import timezone
 from datetime import timedelta
 from .models import Voter
 
+otp = 0;
+timestamp = timezone.now();
+
 def sendEmailOTP(email):
+    global otp, timestamp
     subject = 'Your email verification email'
     otp = random.randint(100000, 999999)
     message = 'Your otp is ' + str(otp)
@@ -14,17 +18,18 @@ def sendEmailOTP(email):
     # Save the OTP along with the timestamp
     user_obj = Voter.objects.get(email=email)
     user_obj.otp = otp
-    user_obj.otp_timestamp = timezone.now()
-    user_obj.save()
+    timestamp = timezone.now()
+    # user_obj.save()
 
     # Send the email
     send_mail(subject, message, from_email, [email])
 
 def verifyEmailOTP(email, entered_otp):
+    global otp, timestamp
     try:
         user_obj = Voter.objects.get(email=email)
-        stored_otp = user_obj.otp
-        otp_timestamp = user_obj.otp_timestamp
+        stored_otp = otp
+        otp_timestamp = timestamp
 
         # Check if OTP_timestamp is not None before performing timedelta
         if otp_timestamp:
@@ -33,7 +38,7 @@ def verifyEmailOTP(email, entered_otp):
 
             if current_time <= valid_timeframe:
                 # OTP is valid
-                return True
+                return (True, stored_otp, otp_timestamp)
 
         # OTP has expired or is incorrect
         return False
